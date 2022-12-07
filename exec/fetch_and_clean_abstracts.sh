7f71916e6d211689dbcc12a2807901423ed27fc3
Biogitte <birgittenilsson@live.dk>
2022-12-07 10:13:20 +0100

    Update exec with abstract cleaning

--- exec/fetch_and_clean_abstracts.sh
+++ exec/fetch_and_clean_abstracts.sh
@@ -4,6 +4,9 @@ echo 'Fetching PubMed abstracts based on the query:' "$QUERY"
 
 python3 "$FETCH_ABSTRACTS" "$DATA_RAW" "$EMAIL" "$QUERY" "$QUERY"
 
+echo 'Cleaning abstracts.'
+python3 "$CLEAN_ABSTRACTS" "$RAW_FILENAME" "$CLEAN_FILENAME"
+
 
 
 
