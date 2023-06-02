7a4f09a6648b6e2687fc9370aafc5291412fee08
Biogitte <birgittenilsson@live.dk>
2023-06-02 19:42:02 +0200

    Update with pyspark and jupyter variables

--- global_env.sh
+++ global_env.sh
@@ -3,10 +3,22 @@
 # Working directory - remember to specify accordingly.
 export WORK_DIR=$PWD
 
+# Setting up Spark
+export SPARK_HOME=/opt/spark
+export PATH=$SPARK_HOME/bin:$PATH
+# Setting up PySpark Jupyter Notebook driver
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
+
 # Data directories
+mkdir -p $WORK_DIR/data
 export DATA=$WORK_DIR/data
+mkdir -p $DATA/raw
 export DATA_RAW=$DATA/raw
-export DATA_PROC=$DATA/processed
+mkdir -p $DATA/proc
+export DATA_PROC=$DATA/proc
+mkdir -p $DATA/results
+export DATA_RESULTS=$DATA/results
 
 # Source code directory
 export SRC=$WORK_DIR/src
@@ -24,3 +36,6 @@ export QUERY='diabetes'
 
 # Variables related to data cleaning and preparation of PubMed abstracts
 export CLEAN_ABSTRACTS=$SRC/clean_abstracts.py
+
+# Executables to fetch and clean abstracts
+export FETCH_AND_CLEAN=$EXEC/fetch_and_clean_abstracts.sh
